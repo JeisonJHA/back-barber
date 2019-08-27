cd3323793e83d1ed0fe1719fb934df542e1115f3
Jeison Azevedo <j3ison@gmail.com>
2019-08-26 23:35:54 -0300

    file controller

---
+++ readme.md
@@ -0,0 +1,8 @@
+# yarn add multer
+***
+# yarn sequelize migration:create --name=creaste-files
+***
+# yarn sequelize db:migrate
+***
+# yarn sequelize migration:create --name=add-avatar-to-user
+***
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import auth from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
 routes.use(auth);
 routes.put('/users', UserController.update);
+routes.put('/files', upload.single('file'), FileController.update);
 
 export default routes;
